35056a14e927d6a2f02ac673d29ae1b3e7dd238d
Ken Shabby <brendan@sonic.net>
2025-08-03 15:04:40 -0700

    Reactor as suggested.

--- repl.go
+++ repl.go
@@ -2,189 +2,79 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"strings"
+
+	"github.com/bootdotdev/pokedexcli/internal/pokeapi"
 )
 
-func startRepl() {
+type config struct {
+	pokeapiClient    pokeapi.Client
+	nextLocationsURL *string
+	prevLocationsURL *string
+}
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	// Beginning of commands section
-	commands := make(map[string]cliCommand)
-	cfg := &config{}
-	cfg.Next = "https://pokeapi.co/api/v2/location-area/"
-
-	commands["exit"] = cliCommand{
-		name:        "exit",
-		description: "Exit the Pokedex",
-		callback: func() error {
-			return commandExit(cfg)
-		},
-	}
-	commands["help"] = cliCommand{
-		name:        "help",
-		description: "Displays a help message",
-		callback: func() error {
-			return commandHelp(cfg, commands)
-		},
-	}
-	commands["map"] = cliCommand{
-		name:        "map",
-		description: "Displays the next 20 location-areas",
-		callback: func() error {
-			return commandMap(cfg)
-		},
-	}
-	commands["mapb"] = cliCommand{
-		name:        "mapb",
-		description: "Displays the previous 20 location-areas",
-		callback: func() error {
-			return commandMapb(cfg)
-		},
-	}
-
-	// Main REPL Loop
+func startRepl(cfg *config) {
+	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		command := scanner.Text()
-		commandSlice := cleanInput(command)
+		reader.Scan()
 
-		commandStruct := commands[commandSlice[0]]
-		if commandStruct.callback == nil {
-			fmt.Println("Unknown command")
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
 			continue
 		}
-		if commandStruct.callback != nil {
-			commandStruct.callback()
+
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback(cfg)
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
 		}
 	}
 }
 
 func cleanInput(text string) []string {
-
-	lower := strings.ToLower(text)
-	words := strings.Fields(lower)
-
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
 	return words
 }
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
 }
 
-func commandExit(c *config) error {
-	fmt.Println("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp(c *config, commands map[string]cliCommand) error {
-	fmt.Println()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage: ")
-	fmt.Println()
-
-	for _, key := range commands {
-		fmt.Println(key.name+": ", key.description)
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Get the next page of locations",
+			callback:    commandMapf,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Get the previous page of locations",
+			callback:    commandMapb,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
 	}
-
-	return nil
-}
-
-type config struct {
-	Next     string
-	Previous string
-}
-
-type locationArea struct {
-	Name string `json:"name"`
-}
-
-type locationAreaResponse struct {
-	Count    int            `json:"count"`
-	Next     string         `json:"next"`
-	Previous string         `json:"previous"`
-	Results  []locationArea `json:"results"`
-}
-
-func commandMap(c *config) error {
-	// Add the map command. It displays the names of 20 location areas in the
-
-	res, err := http.Get(c.Next)
-	if err != nil {
-		log.Println(err)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Printf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-	}
-	if err != nil {
-		log.Println(err)
-	}
-
-	resp := locationAreaResponse{}
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		log.Println(err)
-	}
-
-	c.Next = resp.Next
-	c.Previous = resp.Previous
-
-	for _, area := range resp.Results {
-		fmt.Println(area.Name)
-	}
-
-	return nil
-}
-
-func commandMapb(c *config) error {
-
-	// Do the same as commandMap, but in reverse
-
-	// Make sure there is a previous page
-	if c.Previous == "" {
-		fmt.Println("you're on the first page")
-		return nil
-	}
-
-	res, err := http.Get(c.Previous)
-	if err != nil {
-		log.Println(err)
-	}
-
-	body, err := io.ReadAll(res.Body)
-	defer res.Body.Close()
-	if res.StatusCode > 299 {
-		log.Printf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
-	}
-	if err != nil {
-		log.Println(err)
-	}
-
-	resp := locationAreaResponse{}
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		log.Println(err)
-	}
-
-	c.Next = resp.Next
-	c.Previous = resp.Previous
-
-	for _, area := range resp.Results {
-		fmt.Println(area.Name)
-	}
-
-	return nil
 }
